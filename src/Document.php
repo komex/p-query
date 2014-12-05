74b5c4053c0ad4455ddfeac42ca4939fa0bb2115
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-12-05 18:51:24 +0300

    Dev

--- src/Document.php
+++ src/Document.php
@@ -7,8 +7,6 @@
 
 namespace PQuery;
 
-use PQuery\LayoutFilter\FileLayoutFilter;
-
 /**
  * Class Document
  *
@@ -22,26 +20,162 @@ class Document
      */
     private $stream;
     /**
-     * @var FileLayoutFilter
+     * @var array
      */
-    private $filter;
+    private $namespaces = [];
+    /**
+     * @var array
+     */
+    private $classes = [];
+    /**
+     * @var array
+     */
+    private $methods = [];
 
     /**
      * @param \ArrayIterator $stream
      */
     public function __construct(\ArrayIterator $stream)
     {
+        $this->initDocument($stream);
         $this->stream = $stream;
-        $this->filter = new FileLayoutFilter($stream);
     }
 
     /**
-     * @param string|null $mask
-     *
-     * @return NamespaceLayout
+     * @return array
      */
-    public function getNamespace($mask = null)
+    public function getNamespaces()
     {
-        return new NamespaceLayout($this->stream, $this->filter, $mask);
+        return $this->namespaces;
+    }
+
+    /**
+     * @return array
+     */
+    public function getClasses()
+    {
+        return $this->classes;
+    }
+
+    /**
+     * @return array
+     */
+    public function getMethods()
+    {
+        return $this->methods;
+    }
+
+    /**
+     * @param \ArrayIterator $stream
+     */
+    private function initDocument(\ArrayIterator $stream)
+    {
+        $stream->rewind();
+        while ($stream->valid() === true) {
+            if (is_array($stream->current()) === true) {
+                list($code) = $this->getToken($stream->current());
+                switch ($code) {
+                    case T_FUNCTION:
+                        $this->registerMethod($stream);
+                        break;
+                    case T_CLASS:
+                        $this->registerClass($stream);
+                        break;
+                    case T_NAMESPACE:
+                        $this->registerNamespace($stream);
+                        break;
+                }
+            }
+            $stream->next();
+        }
+    }
+
+    /**
+     * @param \ArrayIterator $stream
+     */
+    private function registerMethod(\ArrayIterator $stream)
+    {
+        assert('$this->getToken($stream->current())[0] === T_FUNCTION');
+        $position = $stream->key();
+        $currentNamespace = empty($this->namespaces) === true ? '\\' : $this->namespaces[0]['name'];
+        $stream->seek($position + 1);
+
+        $name = $this->extractName($stream);
+        if (empty($name) === true || empty($this->classes) === true) {
+            return;
+        }
+        $methodDefinition = [
+            'position' => $position,
+            'namespace' => $currentNamespace,
+            'class' => $this->classes[0]['name'],
+            'name' => $name,
+        ];
+        array_unshift($this->methods, $methodDefinition);
+    }
+
+    /**
+     * @param \ArrayIterator $stream
+     */
+    private function registerClass(\ArrayIterator $stream)
+    {
+        assert('$this->getToken($stream->current())[0] === T_CLASS');
+        $position = $stream->key();
+        $stream->seek($position + 1);
+        $currentNamespace = empty($this->namespaces) === true ? '\\' : $this->namespaces[0]['name'];
+        $definition = [
+            'position' => $position,
+            'namespace' => $currentNamespace,
+            'name' => $this->extractName($stream),
+        ];
+        array_unshift($this->classes, $definition);
+    }
+
+    /**
+     * @param \ArrayIterator $stream
+     */
+    private function registerNamespace(\ArrayIterator $stream)
+    {
+        assert('$this->getToken($stream->current())[0] === T_NAMESPACE');
+        $position = $stream->key();
+        $stream->seek($position + 2);
+        array_unshift($this->namespaces, ['position' => $position, 'name' => $this->extractName($stream)]);
+    }
+
+    /**
+     * @param \ArrayIterator $stream
+     *
+     * @return string
+     */
+    private function extractName(\ArrayIterator $stream)
+    {
+        $name = '';
+        while ($stream->valid() === true) {
+            list($code, $value) = $this->getToken($stream->current());
+            if ($code === T_STRING || $code === T_NS_SEPARATOR) {
+                $name .= $value;
+            } elseif ($code !== T_WHITESPACE) {
+                break;
+            }
+            $stream->next();
+        }
+
+        return $name;
+    }
+
+    /**
+     * @param array|string $token
+     *
+     * @return array
+     */
+    private function getToken($token)
+    {
+        if (is_array($token) === true) {
+            list($code, $value) = $token;
+        } else {
+            $code = null;
+            $value = $token;
+        }
+
+        return [$code, $value];
     }
 }
--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -18,12 +18,48 @@ use PQuery\Document;
 class DocumentTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var \ArrayIterator
+     */
+    private static $stream;
+
+    /**
+     * Init test
+     */
+    public static function setUpBeforeClass()
+    {
+        self::$stream = new \ArrayIterator(token_get_all(file_get_contents(__FILE__)));
+    }
+
+    /**
      * Test count of PHP places.
      */
-    public function testCount()
+    public function testNamespaces()
     {
-        $stream = new \ArrayIterator(token_get_all(file_get_contents(__FILE__)));
-        $document = new Document($stream);
-        $document->getNamespace();
+        $document = new Document(self::$stream);
+        $namespaces = $document->getNamespaces();
+        $this->assertCount(1, $namespaces);
+        $this->assertSame(__NAMESPACE__, $namespaces[0]['name']);
+    }
+
+    /**
+     *
+     */
+    public function testClasses()
+    {
+        $document = new Document(self::$stream);
+        $classes = $document->getClasses();
+        $this->assertCount(1, $classes);
+        $this->assertSame(__NAMESPACE__, $classes[0]['namespace']);
+        $this->assertSame(__CLASS__, $classes[0]['namespace'] . '\\' . $classes[0]['name']);
+    }
+
+    /**
+     *
+     */
+    public function testMethods()
+    {
+        $document = new Document(self::$stream);
+        $methods = $document->getMethods();
+        $this->assertCount(4, $methods);
     }
 }
