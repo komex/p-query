552f467b493dd23b59c55d5ed64062cae3641231
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-12-16 16:13:44 +0300

    FunctionBlockParser

--- src/Parser/FunctionBlockParser.php
+++ src/Parser/FunctionBlockParser.php
@@ -25,15 +25,60 @@ class FunctionBlockParser extends AbstractBlockParser
     public function extract(Stream $stream)
     {
         $block = new FunctionBlock();
-        $position = $stream->key();
-        $block->setPosition($position);
-        $block->setStart($position);
-        $stream->seek($position + 1);
+        $block->setPosition($stream->key());
+        $this->extractAttributes($stream, $block);
+
+        $stream->seek($block->getPosition() + 1);
         $name = $this->extractName($stream);
         if (empty($name) === false) {
             $block->setName($name);
         }
 
         return $block;
+    }
+
+    /**
+     * @param Stream $stream
+     * @param FunctionBlock $block
+     */
+    private function extractAttributes(Stream $stream, FunctionBlock $block)
+    {
+        $stream->prev();
+        while ($stream->valid() === true) {
+            list($code, $value) = $stream->current();
+            switch ($code) {
+                case T_PUBLIC:
+                    $block->setPublic();
+                    break;
+                case T_PROTECTED:
+                    $block->setProtected();
+                    break;
+                case T_PRIVATE:
+                    $block->setPrivate();
+                    break;
+                case T_STATIC:
+                    $block->setStatic();
+                    break;
+                case T_ABSTRACT:
+                    $block->setAbstract();
+                    break;
+                case T_DOC_COMMENT:
+                    $block->setDocBlock($value);
+                    break;
+                case T_FINAL:
+                    $block->setFinal();
+                    break;
+                case T_WHITESPACE:
+                    break;
+                default:
+                    break 2;
+            }
+            $stream->prev();
+        }
+        if ($block->getPosition() - $stream->key() > 2) {
+            $block->setStart($stream->key());
+        } else {
+            $block->setStart($block->getPosition());
+        }
     }
 }
