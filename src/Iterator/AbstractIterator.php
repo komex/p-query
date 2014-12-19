3508930f3dc6f8a20ee57735701bf56350dfd0e8
Андрей Колченко <a.j.kolchenko@baltsoftservice.ru>
2014-12-19 18:06:28 +0300

    Refactor ClassIterator

---
+++ src/Iterator/AbstractIterator.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This file is a part of p-query project.
+ *
+ * (c) Andrey Kolchenko <andrey@kolchenko.me>
+ */
+
+namespace PQuery\Iterator;
+
+use PQuery\Parser\Stream;
+
+/**
+ * Class AbstractIterator
+ *
+ * @package PQuery\Iterator
+ * @author Andrey Kolchenko <andrey@kolchenko.me>
+ */
+abstract class AbstractIterator implements \Iterator, \Countable
+{
+    /**
+     * @var Stream
+     */
+    protected $stream;
+    /**
+     * @var \ArrayIterator[]
+     */
+    protected $elements;
+
+    /**
+     * @param Stream $stream
+     * @param \ArrayIterator $elements
+     */
+    public function __construct(Stream $stream, \ArrayIterator $elements)
+    {
+        $this->stream = $stream;
+        $this->elements = $elements;
+    }
+
+    /**
+     * @return ClassIterator
+     */
+    public function current()
+    {
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function next()
+    {
+        $this->getElement()->next();
+    }
+
+    /**
+     * @return int
+     */
+    public function key()
+    {
+        return $this->getElement()->key();
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid()
+    {
+        return $this->getElement()->valid();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rewind()
+    {
+        $this->getElement()->rewind();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function count()
+    {
+        return $this->getElement()->count();
+    }
+
+    /**
+     * @return \ArrayIterator
+     */
+    abstract protected function getElement();
+}
--- src/Iterator/ClassIterator.php
+++ src/Iterator/ClassIterator.php
@@ -7,68 +7,20 @@
 
 namespace PQuery\Iterator;
 
-use PQuery\Parser\Stream;
-
 /**
  * Class ClassIterator
  *
  * @package PQuery\Iterator
  * @author Andrey Kolchenko <andrey@kolchenko.me>
  */
-class ClassIterator implements \Iterator, \Countable
+class ClassIterator extends AbstractLayoutIterator
 {
     /**
-     * @var Stream
+     * @return \ArrayIterator
      */
-    private $stream;
-    /**
-     * @var \ArrayIterator[]
-     */
-    private $elements;
-
-    /**
-     * @param Stream $stream
-     * @param \ArrayIterator $elements
-     */
-    public function __construct(Stream $stream, \ArrayIterator $elements)
+    protected function getElement()
     {
-        $this->stream = $stream;
-        $this->elements = $elements;
-    }
-
-    /**
-     * @return string
-     */
-    public function getName()
-    {
-        $this->stream->seek($this->elements[T_CLASS]->key() + 2);
-        while ($this->stream->valid() === true) {
-            list($code, $value) = $this->stream->current();
-            if ($code === T_STRING) {
-                return $value;
-            }
-            $this->stream->next();
-        }
-        throw new \RuntimeException('Unexpected end of stream.');
-    }
-
-    /**
-     * @return bool
-     */
-    public function isAbstract()
-    {
-        $this->stream->seek($this->elements[T_CLASS]->key() - 1);
-        while ($this->stream->valid() === true) {
-            list($code) = $this->stream->current();
-            if ($code === T_ABSTRACT) {
-                return true;
-            } elseif ($code !== T_WHITESPACE && $code !== T_FINAL) {
-                break;
-            }
-            $this->stream->prev();
-        }
-
-        return false;
+        return $this->elements[T_CLASS];
     }
 
     /**
@@ -76,66 +28,6 @@ class ClassIterator implements \Iterator, \Countable
      */
     public function isFinal()
     {
-        $this->stream->seek($this->elements[T_CLASS]->key() - 1);
-        while ($this->stream->valid() === true) {
-            list($code) = $this->stream->current();
-            if ($code === T_FINAL) {
-                return true;
-            } elseif ($code !== T_WHITESPACE && $code !== T_ABSTRACT) {
-                break;
-            }
-            $this->stream->prev();
-        }
-
-        return false;
-    }
-
-    /**
-     * @return ClassIterator
-     */
-    public function current()
-    {
-        return $this;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function next()
-    {
-        $this->elements[T_CLASS]->next();
-    }
-
-    /**
-     * @return int
-     */
-    public function key()
-    {
-        return $this->elements[T_CLASS]->key();
-    }
-
-    /**
-     * @return bool
-     */
-    public function valid()
-    {
-        return $this->elements[T_CLASS]->valid();
-    }
-
-
-    /**
-     * @inheritdoc
-     */
-    public function rewind()
-    {
-        $this->elements[T_CLASS]->rewind();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function count()
-    {
-        return $this->elements[T_CLASS]->count();
+        return $this->isAttributeExists(T_FINAL, [T_WHITESPACE, T_ABSTRACT]);
     }
 }
