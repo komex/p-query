b77e1d26930fba71cbcafcae83ec7eb434030c9e
Andrey Kolchenko <andrey@kolchenko.me>
2015-05-23 15:33:51 +0300

    Doen ParserInterface

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -15,5 +15,42 @@ namespace Komex\Perk;
  */
 interface ParserInterface
 {
+    /**
+     * The token is unknown
+     */
+    const UNKNOWN = 0;
+    /**
+     * The token is matched
+     */
+    const MATCH = 1;
+    /**
+     * Bad sequence
+     */
+    const RESET = 2;
+    /**
+     * The sequence is done
+     */
+    const DONE = 3;
 
+    /**
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * @return callable[]
+     */
+    public function getTokensMap();
+
+    /**
+     * @return callable[]
+     */
+    public function getValuesMap();
+
+    /**
+     * @param callable $handler
+     *
+     * @return string
+     */
+    public function onMatch(callable $handler = null);
 }
